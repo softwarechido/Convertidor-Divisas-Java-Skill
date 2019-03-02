3242f9e92a2c04859d2dfe43bbde977b453b2531
Sinuhe Jaime <sierisimo@gmail.com>
2019-03-02 02:14:18 -0600

    More in the idiomatic stuff

--- src/main/kotlin/convertidordivisas/ConvertidorDivisasStreamHandler.kt
+++ src/main/kotlin/convertidordivisas/ConvertidorDivisasStreamHandler.kt
@@ -3,18 +3,23 @@ package convertidordivisas
 import com.amazon.ask.Skill
 import com.amazon.ask.SkillStreamHandler
 import com.amazon.ask.Skills
+import com.amazon.ask.builder.CustomSkillBuilder
+import com.amazon.ask.dispatcher.request.handler.HandlerInput
+import com.amazon.ask.model.Response
+import com.amazon.ask.request.handler.GenericRequestHandler
 import convertidordivisas.handlers.*
+import java.util.*
 
-private val skill: Skill
-    get() {
-        return with(Skills.custom()) {
-            addRequestHandler(CancelandStopIntentHandler())
-            addRequestHandler(ConvertIntentHandler())
-            addRequestHandler(HelpIntentHandler())
-            addRequestHandler(LaunchRequestHandler())
-            addRequestHandler(SessionEndedRequestHandler())
-            build()
-        }
-    }
+class ConvertidorDivisasStreamHandler : SkillStreamHandler(skill {
+    addRequestHandler(CancelandStopIntentHandler())
+    addRequestHandler(ConvertIntentHandler())
+    addRequestHandler(HelpIntentHandler())
+    addRequestHandler(LaunchRequestHandler())
+    addRequestHandler(SessionEndedRequestHandler())
+})
 
-class ConvertidorDivisasStreamHandler : SkillStreamHandler(skill)
+inline fun skill(buildSkillBlock: CustomSkillBuilder.() -> Unit): Skill {
+    val customSkill = Skills.custom()
+    customSkill.buildSkillBlock()
+    return customSkill.build()
+}
