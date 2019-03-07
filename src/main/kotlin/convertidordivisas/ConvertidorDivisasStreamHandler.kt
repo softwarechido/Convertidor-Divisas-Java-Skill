1c12b26d826330560884002645c8668be39985cf
Sinuhe Jaime <sinuhe.jaimevalencia@cognizant.com>
2019-03-07 01:44:28 -0500

    Weird example of dinamyc creation

--- src/main/kotlin/convertidordivisas/ConvertidorDivisasStreamHandler.kt
+++ src/main/kotlin/convertidordivisas/ConvertidorDivisasStreamHandler.kt
@@ -1,7 +1,10 @@
 package convertidordivisas
 
 import com.amazon.ask.SkillStreamHandler
+import com.amazon.ask.request.Predicates.intentName
+import convertidordivisas.dsl.buildHandler
 import convertidordivisas.dsl.handler
+import convertidordivisas.dsl.response
 import convertidordivisas.dsl.skill
 import convertidordivisas.handlers.*
 
@@ -11,4 +14,21 @@ class ConvertidorDivisasStreamHandler : SkillStreamHandler(skill {
     handler { HelpIntentHandler() }
     handler { LaunchRequestHandler() }
     handler { SessionEndedRequestHandler() }
+
+    /**
+     * Weird experiment for dynamic generation…
+     */
+    handler {
+        buildHandler {
+            predicate {
+                matches(intentName("SierIntent"))
+            }
+
+            handler = {
+                response(this) {
+                    speech = "Yeah… this won't be called"
+                }
+            }
+        }
+    }
 })
--- src/main/kotlin/convertidordivisas/dsl/skillDSL.kt
+++ src/main/kotlin/convertidordivisas/dsl/skillDSL.kt
@@ -4,6 +4,7 @@ import com.amazon.ask.Skill
 import com.amazon.ask.Skills
 import com.amazon.ask.builder.CustomSkillBuilder
 import com.amazon.ask.dispatcher.request.handler.HandlerInput
+import com.amazon.ask.dispatcher.request.handler.RequestHandler
 import com.amazon.ask.model.Response
 import com.amazon.ask.request.handler.GenericRequestHandler
 import java.util.*
@@ -22,4 +23,31 @@ inline fun skill(buildSkillBlock: CustomSkillBuilder.() -> Unit): Skill {
  */
 inline fun CustomSkillBuilder.handler(block: () -> GenericRequestHandler<HandlerInput, Optional<Response>>) {
     addRequestHandler(block())
+}
+
+/**
+ * Weird set of elements for a weird experiment…
+ */
+@DslMarker
+annotation class DynamicSkill
+
+@DynamicSkill
+class HandlerBuilder(var predicate: HandlerInput.() -> Boolean = { false },
+                     var handler: HandlerInput.() -> Optional<Response> = { Optional.empty() }) {
+    fun predicate(block: HandlerInput.() -> Boolean) {
+        this.predicate = block
+    }
+}
+
+inline fun buildHandler(buildBlock: HandlerBuilder.() -> Unit): RequestHandler {
+    val handlerBuilder = HandlerBuilder()
+    handlerBuilder.buildBlock()
+
+    return object : RequestHandler {
+        override fun canHandle(input: HandlerInput): Boolean =
+                handlerBuilder.predicate(input)
+
+        override fun handle(input: HandlerInput): Optional<Response> =
+                handlerBuilder.handler(input)
+    }
 }
